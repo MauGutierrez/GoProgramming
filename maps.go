4fc40108eaf1cf5477172aa4027ae579b6654a39
Gerardo Gutierrez <gerardogutierrez@Base22s-MacBook-Pro.local>
2020-04-27 16:04:06 -0500

    First commit

---
+++ maps.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := make(map[string]int)
+
+	m["k1"] = 7
+	m["k2"] = 13
+
+	fmt.Println("map:", m)
+
+	v1 := m["k1"]
+	fmt.Println("v1:", v1)
+
+	fmt.Println("len:", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map:", m)
+
+	/* 
+		The second return value is used to indicate
+		if a key was present in a map. 
+		In this case, we search for the K2 key 
+	*/
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
+}
